929fc01b475fa793f9f9eb93935cbf3acfe3ef00
Breno Pereira <brenomartinsxd@gmail.com>
2022-05-15 23:52:04 -0400

    Remove duplicate folders in app

--- Pods/Target Support Files/Pods-NotePad-NotePadTests/Pods-NotePad-NotePadTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NotePad_NotePadTests : NSObject
-@end
-@implementation PodsDummy_Pods_NotePad_NotePadTests
-@end
--- Pods/Target Support Files/Pods-NotePad/Pods-NotePad-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NotePad : NSObject
-@end
-@implementation PodsDummy_Pods_NotePad
-@end
